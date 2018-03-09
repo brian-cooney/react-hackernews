c0ec66dba401c53ac4a737b99603b02a128fa2b1
brian-cooney <bcooney@gmail.com>
2018-03-09 10:56:15 +0000

    Map over fake sample data

--- src/App.js
+++ src/App.js
@@ -2,17 +2,44 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react',
+    author: 'Jordon Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://facebook.github.io/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 0,
+  },
+];
+
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {list.map(function(item) {
+          return (
+            <div>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+            </div>
+          );
+        })}
       </div>
     );
   }
