3ce74b82f28178a9ac82d98d74dd80f2d322fca9
brian-cooney <bcooney@gmail.com>
2018-05-01 09:22:29 +0100

    arrow function example inside of onClick event

--- src/App.js
+++ src/App.js
@@ -50,6 +50,7 @@ class App extends Component {
               <span>{item.author}</span>
               <span>{item.num_comments}</span>
               <span>{item.points}</span>
+              <span>{item.points}</span>
               <span>
                 <button
                 onClick={() => this.onDismiss(item.objectID)}
